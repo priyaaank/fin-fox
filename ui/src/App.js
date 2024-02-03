d757a4a843b9a2411c4cd76953bedc2434ca70c0
Priyank Gupta <priyaaank@gmail.com>
2024-02-03 11:31:48 +0530

    Adding basic reactjs app for the UI layer

---
+++ ui/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Simple React App</h1>
+        </header>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
